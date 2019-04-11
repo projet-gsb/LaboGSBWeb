d77dfc321dea80d4810b45c46178e707758396a0
Firost Tolundur <Tolundur@outlook.com>
2019-04-11 16:22:13 +0200

    dataReader

--- GSB/Models/DAO/EtablissementDAO.cs
+++ GSB/Models/DAO/EtablissementDAO.cs
@@ -51,7 +51,7 @@ namespace LaboGSB.Models.DAO.DAOCompteRendu
 
                 etablissement = new Etablissement(idEtablissement, nom, adresse, numeroTelephone, mel, typeEtab);
             }
-            dataReader.Close();
+            // dataReader.Close();
 
             return etablissement;
         }
