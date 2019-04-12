9c9b8f79f1ae9cd0aaf35cad4f3b7afc02a44ed8
admin <admin@BTSWIN7-99>
2019-04-12 10:39:11 +0200

    Finition Fiche de frais

--- GSB/Models/BO/FicheDeFrais.cs
+++ GSB/Models/BO/FicheDeFrais.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 
 namespace LaboGSB.Models.BO.BOGestionFrais
-{ 
+{
     public class FicheDeFrais
     {
-        public int Id { get; set; }
-        public int IdVisiteurMedical { get; set; }
-        public DateTime DateCreation { get; protected set; }
-        public DateTime DateTraitement { get; protected set; }
-        public bool MiseEnPaiement { get; protected set; }
+        private int _id;
+        private int _idVisiteurMedical;
+        private DateTime _dateCreation;
+        private DateTime _dateTraitement;
+        private bool _miseEnPaiement;
+
+        public int Id { get => _id; set => _id = value; }
+        public int IdVisiteurMedical { get => _idVisiteurMedical; set => _idVisiteurMedical = value; }
+        public DateTime DateCreation { get => _dateCreation; protected set => _dateCreation = value; }
+        public DateTime DateTraitement { get => _dateTraitement; protected set => _dateTraitement = value; }
+        public bool MiseEnPaiement { get => _miseEnPaiement; protected set => _miseEnPaiement = value; }
 
         public FicheDeFrais(int id, int idvisiteurmedical, DateTime datecreation, DateTime datetraitement, bool miseenpaiement)
         {
