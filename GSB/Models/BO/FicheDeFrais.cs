ddc03c667be8d916b4a86abfa931e837bacc34af
admin <admin@BTSWIN7-99>
2019-04-19 18:28:35 +0200

    push Partie Frais encore

--- GSB/Models/BO/FicheDeFrais.cs
+++ GSB/Models/BO/FicheDeFrais.cs
@@ -9,15 +9,15 @@ namespace LaboGSB.Models.BO.BOGestionFrais
     public class FicheDeFrais
     {
         private int _id;
-        //private int _idVisiteurMedical;  
-        private VisiteurMedical _visiteurMedicalConcerne;
+        private int _idVisiteurMedical;  
+        //private VisiteurMedical _visiteurMedicalConcerne;
         private DateTime _dateCreation;
         private DateTime _dateTraitement;
         private bool _miseEnPaiement;
 
         public int Id { get => _id; set => _id = value; }
-        //public int IdVisiteurMedical { get => _idVisiteurMedical; set => _idVisiteurMedical = value; }
-        public VisiteurMedical VisiteurMedicalConcerne { get => _visiteurMedicalConcerne; set => _visiteurMedicalConcerne = value; }
+        public int IdVisiteurMedical { get => _idVisiteurMedical; set => _idVisiteurMedical = value; }
+        //public VisiteurMedical VisiteurMedicalConcerne { get => _visiteurMedicalConcerne; set => _visiteurMedicalConcerne = value; }
         public DateTime DateCreation { get => _dateCreation; protected set => _dateCreation = value; }
         public DateTime DateTraitement { get => _dateTraitement; protected set => _dateTraitement = value; }
         public bool MiseEnPaiement { get => _miseEnPaiement; protected set => _miseEnPaiement = value; }
@@ -25,18 +25,18 @@ namespace LaboGSB.Models.BO.BOGestionFrais
         public FicheDeFrais()
         {
             this.Id = 0;
-            //this.IdVisiteurMedical = 0;           //à modifier par ID du visiteur connecté
-            this.VisiteurMedicalConcerne = new VisiteurMedical();
+            this.IdVisiteurMedical = 0;           //à modifier par ID du visiteur connecté
+            //this.VisiteurMedicalConcerne = new VisiteurMedical();
             this.DateCreation = new DateTime();
             this.DateTraitement = new DateTime();
             this.MiseEnPaiement = false;
         }
                        
-        public FicheDeFrais(int id, VisiteurMedical visiteurMedical, DateTime datecreation, DateTime datetraitement, bool miseenpaiement)
+        public FicheDeFrais(int id, int idvisiteurmedical, DateTime datecreation, DateTime datetraitement, bool miseenpaiement)
         {
             this.Id = id;
-            //this.IdVisiteurMedical = idvisiteurmedical;
-            this.VisiteurMedicalConcerne = new VisiteurMedical();
+            this.IdVisiteurMedical = idvisiteurmedical;
+            //this.VisiteurMedicalConcerne = new VisiteurMedical();
             this.DateCreation = datecreation;
             this.DateTraitement = datetraitement;
             this.MiseEnPaiement = miseenpaiement;
